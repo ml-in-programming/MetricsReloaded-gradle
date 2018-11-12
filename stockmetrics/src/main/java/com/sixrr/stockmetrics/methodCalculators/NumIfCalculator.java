e8876b8b13569f1ceaa05dc8db38664443e8dfc4
Gleb Zakharov <snyssfx@gmail.com>
2018-11-12 11:49:09 +0300

    add metric for counting 'if' statements

---
+++ stockmetrics/src/main/java/com/sixrr/stockmetrics/methodCalculators/NumIfCalculator.java
@@ -0,0 +1,39 @@
+package com.sixrr.stockmetrics.methodCalculators;
+
+import com.intellij.psi.JavaRecursiveElementVisitor;
+import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.PsiIfStatement;
+import com.intellij.psi.PsiMethod;
+import com.sixrr.metrics.utils.MethodUtils;
+
+public class NumIfCalculator extends MethodCalculator {
+    private int methodNestingDepth = 0;
+    private int elementCount = 0;
+
+    @Override
+    protected PsiElementVisitor createVisitor() {
+        return new NumIfCalculator.Visitor();
+    }
+
+    private class Visitor extends JavaRecursiveElementVisitor {
+
+        @Override
+        public void visitMethod(PsiMethod method) {
+            if (methodNestingDepth == 0) {
+                elementCount = 0;
+            }
+            methodNestingDepth++;
+            super.visitMethod(method);
+            methodNestingDepth--;
+            if (methodNestingDepth == 0 && !MethodUtils.isAbstract(method)) {
+                postMetric(method, elementCount);
+            }
+        }
+
+        @Override
+        public void visitIfStatement(PsiIfStatement statement) {
+            super.visitIfStatement(statement);
+            elementCount++;
+        }
+    }
+}
