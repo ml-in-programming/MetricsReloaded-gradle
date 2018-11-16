1dcf115f85de70c510142611a13eb2a6e54e121e
Gleb Zakharov <snyssfx@gmail.com>
2018-11-16 18:31:36 +0300

    metrics: add number of local vars access metric

---
+++ stockmetrics/src/main/java/com/sixrr/stockmetrics/methodCalculators/NumLocalVarsAccessCalculator.java
@@ -0,0 +1,41 @@
+package com.sixrr.stockmetrics.methodCalculators;
+
+import com.intellij.psi.JavaRecursiveElementVisitor;
+import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.PsiLocalVariable;
+import com.intellij.psi.PsiMethod;
+import com.sixrr.metrics.utils.MethodUtils;
+
+public class NumLocalVarsAccessCalculator extends MethodCalculator {
+    private int methodNestingDepth = 0;
+    private int elementCount = 0;
+
+    @Override
+    protected PsiElementVisitor createVisitor() {
+        return new NumLocalVarsAccessCalculator.Visitor();
+    }
+
+    private class Visitor extends JavaRecursiveElementVisitor {
+
+        @Override
+        public void visitMethod(PsiMethod method) {
+            if (methodNestingDepth == 0) {
+                elementCount = 0;
+            }
+
+            methodNestingDepth++;
+            super.visitMethod(method);
+            methodNestingDepth--;
+
+            if (methodNestingDepth == 0 && !MethodUtils.isAbstract(method)) {
+                postMetric(method, elementCount);
+            }
+        }
+
+        @Override
+        public void visitLocalVariable(PsiLocalVariable variable) {
+            super.visitLocalVariable(variable);
+            elementCount++;
+        }
+    }
+}
