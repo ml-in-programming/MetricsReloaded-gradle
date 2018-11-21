1104359d0edff4a2a5417443dc2ff5591a770b5f
Gleb Zakharov <snyssfx@gmail.com>
2018-11-21 14:16:56 +0300

    extract all work for refresh candidates statuses from calculators into CandidateCalculator

--- stockmetrics/src/main/java/com/sixrr/stockmetrics/candidateCalculators/CandidateCalculator.java
+++ stockmetrics/src/main/java/com/sixrr/stockmetrics/candidateCalculators/CandidateCalculator.java
@@ -1,18 +1,22 @@
 package com.sixrr.stockmetrics.candidateCalculators;
 
-import com.intellij.openapi.progress.EmptyProgressIndicator;
-import com.intellij.openapi.progress.ProgressManager;
+import com.intellij.psi.JavaRecursiveElementVisitor;
 import com.intellij.psi.PsiMethod;
 import com.intellij.psi.PsiStatement;
+import com.sixrr.metrics.utils.MethodUtils;
 import com.sixrr.stockmetrics.execution.BaseMetricsCalculator;
 import org.jetbrains.research.groups.ml_methods.utils.ExtractionCandidate;
 
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 abstract class CandidateCalculator extends BaseMetricsCalculator {
 
-    ArrayList<ExtractionCandidate> candidates;
-    ArrayList<ExtractionCandidate> methodCandidates;
+    private ArrayList<ExtractionCandidate> candidates;
+
+    public CandidateCalculator(ArrayList<ExtractionCandidate> candidates) {
+        this.candidates = candidates;
+    }
 
     void postMetric(ExtractionCandidate candidate, int numerator, int denominator) {
         resultsHolder.postCandidateMetric(metric, candidate, (double) numerator, (double) denominator);
@@ -26,8 +30,53 @@ abstract class CandidateCalculator extends BaseMetricsCalculator {
         resultsHolder.postCandidateMetric(metric, candidate, value);
     }
 
-    void setInsideCandidate(PsiStatement currentStatement) {
-        for (ExtractionCandidate candidate: methodCandidates) {
+    public class CandidateVisitor extends JavaRecursiveElementVisitor {
+        private int methodNestingDepth = 0;
+        ArrayList<ExtractionCandidate> methodCandidates;
+        ArrayList<Integer> counts = new ArrayList<>();
+        boolean isInsideMethod = false;
+
+        @Override
+        public void visitMethod(PsiMethod method) {
+            if (methodNestingDepth == 0) {
+                methodCandidates = getCandidatesOfMethod(method, candidates);
+                counts.clear();
+                methodCandidates.forEach(elem -> counts.add(0));
+                isInsideMethod = true;
+            }
+
+            methodNestingDepth++;
+            super.visitMethod(method);
+            methodNestingDepth--;
+
+            if (methodNestingDepth == 0 && !MethodUtils.isAbstract(method)) {
+                for (int i = 0; i < methodCandidates.size(); i++) {
+                    postMetric(methodCandidates.get(i), counts.get(i));
+                }
+                candidates.removeAll(methodCandidates);
+                isInsideMethod = false;
+            }
+        }
+
+        @Override
+        public void visitStatement(PsiStatement statement) {
+            super.visitStatement(statement);
+            setInsideCandidate(statement, methodCandidates);
+        }
+    }
+
+    private static ArrayList<ExtractionCandidate> getCandidatesOfMethod(
+            PsiMethod method,
+            ArrayList<ExtractionCandidate> allCandidates)
+    {
+        return allCandidates.stream()
+                .filter(elem -> elem.getSourceMethod().equals(method))
+                .collect(Collectors.toCollection(ArrayList::new));
+
+    }
+
+    private static void setInsideCandidate(PsiStatement currentStatement, ArrayList<ExtractionCandidate> candidates) {
+        for (ExtractionCandidate candidate: candidates) {
 
             if (candidate.getStart().equals(currentStatement))
                 candidate.setInCandidate(true);
