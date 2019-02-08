944724a8f4b2788d0176d6fb6563f8c00ef2e80c
Gleb Zakharov <snyssfx@gmail.com>
2019-02-08 13:37:40 +0300

    add local vars and typed elem metrics and complement

---
+++ stockmetrics/src/main/java/com/sixrr/stockmetrics/candidateCalculators/NumLocalVarsCalculator.java
@@ -0,0 +1,30 @@
+package com.sixrr.stockmetrics.candidateCalculators;
+
+import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.PsiLocalVariable;
+import org.jetbrains.research.groups.ml_methods.utils.ExtractionCandidate;
+
+import java.util.ArrayList;
+
+public class NumLocalVarsCalculator extends AbstractNumCandidateCalculator {
+
+    public NumLocalVarsCalculator(ArrayList<ExtractionCandidate> candidates) {
+        super(candidates);
+    }
+
+    @Override
+    protected PsiElementVisitor createVisitor() {
+        return new NumLocalVarsCalculator.Visitor();
+    }
+
+    private class Visitor extends CandidateVisitor {
+
+        @Override
+        public void visitLocalVariable(PsiLocalVariable variable) {
+            super.visitLocalVariable(variable);
+            if (isInsideMethod) {
+                updateCounters();
+            }
+        }
+    }
+}
--- stockmetrics/src/main/java/com/sixrr/stockmetrics/candidateMetrics/NumVarsAccessCandidateMetric.java
+++ stockmetrics/src/main/java/com/sixrr/stockmetrics/candidateMetrics/NumVarsAccessCandidateMetric.java
@@ -2,7 +2,6 @@ package com.sixrr.stockmetrics.candidateMetrics;
 
 import com.sixrr.metrics.MetricCalculator;
 import com.sixrr.metrics.MetricType;
-import com.sixrr.stockmetrics.candidateCalculators.NumLocalVarsCalculator;
 import com.sixrr.stockmetrics.candidateCalculators.NumVarsAccessCandidateCalculator;
 import com.sixrr.stockmetrics.i18n.StockMetricsBundle;
 import org.jetbrains.annotations.NotNull;
@@ -10,24 +9,24 @@ import org.jetbrains.research.groups.ml_methods.utils.ExtractionCandidate;
 
 import java.util.ArrayList;
 
-public class NumLocalVarsCandidateMetric extends CandidateMetric {
+public class NumVarsAccessCandidateMetric extends CandidateMetric {
 
     private ArrayList<ExtractionCandidate> candidates;
 
-    public NumLocalVarsCandidateMetric(ArrayList<ExtractionCandidate> candidates) {
+    public NumVarsAccessCandidateMetric(ArrayList<ExtractionCandidate> candidates) {
         this.candidates = candidates;
     }
 
     @NotNull
     @Override
     public String getDisplayName() {
-        return StockMetricsBundle.message("number.of.local.vars.candidates.display.name");
+        return StockMetricsBundle.message("number.of.local.vars.access.candidates.display.name");
     }
 
     @NotNull
     @Override
     public String getAbbreviation() {
-        return StockMetricsBundle.message("number.of.local.vars.candidates.abbreviation");
+        return StockMetricsBundle.message("number.of.local.vars.access.candidates.abbreviation");
     }
 
     @NotNull
@@ -39,6 +38,6 @@ public class NumLocalVarsCandidateMetric extends CandidateMetric {
     @NotNull
     @Override
     public MetricCalculator createCalculator() {
-        return new NumLocalVarsCalculator(candidates);
+        return new NumVarsAccessCandidateCalculator(candidates);
     }
 }
